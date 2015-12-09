bfebc20e431ef987856b0dac8aa47e955b8555db
Filip <filczaj@gmail.com>
2015-12-09 23:42:06 +0100

    Wyświetlanie nutek jeszcze nie działa...

---
+++ MusicAnalyzer/GUI/PlayerWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MusicAnalyzer.GUI
+{
+    /// <summary>
+    /// Interaction logic for PlayWindow.xaml
+    /// </summary>
+    public partial class PlayerWindow : Window
+    {
+        public PlayerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
